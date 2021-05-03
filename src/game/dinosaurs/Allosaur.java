50ba30c56ddf46efcba122943e1047c4c8e01c6b
JONATHAN_WONG_LEONG_SHAN <jwon0100@student.monash.edu>
2021-05-03 23:39:36 +0800

    Added attackedStegos hashmap

--- src/game/dinosaurs/Allosaur.java
+++ src/game/dinosaurs/Allosaur.java
@@ -6,9 +6,12 @@ import edu.monash.fit2099.engine.GameMap;
 import game.attack.AttackAction;
 import game.breed.BreedingAction;
 
+import java.util.HashMap;
+
 public class Allosaur extends DinoActor {
 
     private static final DinoEncyclopedia DINO_TYPE = DinoEncyclopedia.ALLOSAUR;
+    private HashMap<Stegosaur,Integer> attackedStegos = new HashMap<>();
 
     public Allosaur(Sex sex, Boolean isMatured) {
         super(DINO_TYPE, sex, isMatured);
