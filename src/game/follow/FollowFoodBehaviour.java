6de99d11cb3e671b262fa799d27f1e2a5f65f84a
jang0018 <jang0018@student.monash.edu>
2021-05-05 19:09:07 +1000

    Upload FollowFoodBehaviour

---
+++ src/game/follow/FollowFoodBehaviour.java
@@ -0,0 +1,28 @@
+package game.follow;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+import game.dinosaurs.DinoActor;
+import game.dinosaurs.DinoCapabilities;
+
+public class FollowFoodBehaviour extends FollowBehaviour {
+
+    private static final String DESCRIPTION = "find food";
+
+    public FollowFoodBehaviour(DinoCapabilities diet) {
+        super(diet, DESCRIPTION);
+    }
+
+    @Override
+    public Action getAction(Actor actor, GameMap map) {
+        if (actor instanceof DinoActor){
+            DinoCapabilities followPurpose = super.getFollowPurpose();
+            if (followPurpose == DinoCapabilities.HERBIVORE || followPurpose == DinoCapabilities.CARNIVORE){
+                super.getAction(actor, map);
+            }
+        }
+        return null;
+    }
+
+}
