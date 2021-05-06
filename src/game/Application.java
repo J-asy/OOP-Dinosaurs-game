f1e27fb2c01cd00e0525637074436e597fa59a5f
JONATHAN_WONG_LEONG_SHAN <jwon0100@student.monash.edu>
2021-05-06 14:52:41 +0800

    Initial hit points correction

--- src/game/Application.java
+++ src/game/Application.java
@@ -9,13 +9,11 @@ import edu.monash.fit2099.engine.FancyGroundFactory;
 import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.World;
 import game.dinosaurs.*;
-import game.environment.*;
-//import game.environment.Dirt;
-//import game.environment.Floor;
-//import game.environment.Tree;
-//import game.environment.Wall;
+import game.environment.Dirt;
+import game.environment.Floor;
+import game.environment.Tree;
+import game.environment.Wall;
 import game.player.Player;
-import game.player.VendingMachine;
 
 /**
  * The main class for the Jurassic World game.
@@ -26,13 +24,13 @@ public class Application {
 	public static void main(String[] args) {
 		World world = new World(new Display());
 
-		FancyGroundFactory groundFactory = new FancyGroundFactory(new Dirt(), new Wall(), new Floor(), new Tree(),new VendingMachine());
-		
+		FancyGroundFactory groundFactory = new FancyGroundFactory(new Dirt(), new Wall(), new Floor(), new Tree());
+
 		List<String> map = Arrays.asList(
 		"................................................................................",
 		"................................................................................",
 		".....#######....................................................................",
-		".....#__X__#....................................................................",
+		".....#_____#....................................................................",
 		".....#_____#....................................................................",
 		".....###.###....................................................................",
 		"................................................................................",
@@ -56,26 +54,21 @@ public class Application {
 		"................................................................................");
 		GameMap gameMap = new GameMap(groundFactory, map );
 		world.addGameMap(gameMap);
-		
+
 		Actor player = new Player("Player", '@', 100);
 		world.addPlayer(player, gameMap.at(9, 4));
-		
+
 		// Place a pair of stegosaurs in the middle of the map
 //		gameMap.at(30, 12).addActor(new Stegosaur(Sex.FEMALE));
 
 		DinoActor s1 = new Stegosaur(DinoCapabilities.MALE, true);
 		gameMap.at(32, 12).addActor(s1);
-
-		DinoActor a1 = new Allosaur(DinoCapabilities.MALE, true);
-		gameMap.at(32, 13).addActor(a1);
-
-
-//		DinoActor s2 = new Stegosaur(DinoCapabilities.FEMALE, true);
+//		DinoActor s2 = new Stegosaur(Sex.FEMALE, true);
 //		gameMap.at(32, 10).addActor(s2);
-//
-//
+
+
 //		gameMap.at(1,3).addActor(new Stegosaur(true));
-//		DinoActor b = new Brachiosaur(DinoCapabilities.FEMALE, true);
+//		DinoActor b = new Brachiosaur(Sex.FEMALE, true);
 //		gameMap.at(1,1).addActor(b);
 //		DinoActor a = new Allosaur(true);
 //		gameMap.at(1,5).addActor(a);
