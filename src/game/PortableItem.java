5a48f34d490b1bdc21e79bce152b4c330ec5f30e
jang0018 <jang0018@student.monash.edu>
2021-05-15 16:11:15 +0800

    make PortableItem abstract

--- src/game/PortableItem.java
+++ src/game/PortableItem.java
@@ -2,33 +2,10 @@ package game;
 
 import edu.monash.fit2099.engine.Item;
 
-/**
- * Base class for any item that can be picked up and dropped.
- */
-public class PortableItem extends Item {
+public abstract class PortableItem extends Item {
 
-	private int healPoints;
-
-	public PortableItem(String name, char displayChar) {
-		super(name, displayChar, true);
-	}
-
-	public boolean edibleByHerbivores(){
-		return hasCapability(FoodType.HERBIVORE);
-	}
-
-	public boolean edibleByCarnivores(){
-		return hasCapability(FoodType.CARNIVORE);
-	}
-
-	public void setHealPoints(int newHealPoints) {
-		if (newHealPoints > 0){
-			healPoints = newHealPoints;
-		}
-	}
-
-	 public int getHealPoints(){
-		return healPoints;
-	}
+    public PortableItem(String name, char displayChar) {
+        super(name, displayChar, true);
+    }
 
 }
