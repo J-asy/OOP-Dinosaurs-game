51bfd46ff5dea8d17a053416b706e89cf53a5048
jang0018 <jang0018@student.monash.edu>
2021-05-16 13:22:58 +0800

    update MealKitsItem

--- src/game/player/MealKitsItem.java
+++ src/game/player/MealKitsItem.java
@@ -1,13 +1,17 @@
 package game.player;
 
+import edu.monash.fit2099.engine.Location;
 import game.FoodItem;
-import game.FoodType;
-import game.PortableItem;
+import game.dinosaurs.CapableActor;
 
 /**
  * Class represent Meal Kits that can be fed to the dinosaurs.
  */
 public class MealKitsItem extends FoodItem {
+
+    private final static int HERBIVORE_HEAL_POINTS = 100;
+    private final static int CARNIVORE_HEAL_POINTS = 500;
+
     /***
      * Constructor.
      *  @param name the name of this Item
@@ -16,4 +20,21 @@ public class MealKitsItem extends FoodItem {
         super(name, '=');
     }
 
+    @Override
+    public void setForHerbivores() {
+        setHealPoints(HERBIVORE_HEAL_POINTS);
+        super.setForHerbivores();
+    }
+
+    @Override
+    public void setForCarnivores() {
+        setHealPoints(CARNIVORE_HEAL_POINTS);
+        super.setForCarnivores();
+    }
+
+    @Override
+    public boolean canEat(CapableActor capableActor, Location location) {
+        return capableActor.isHerbivorous() && edibleByHerbivores() ||
+                capableActor.isCarnivorous() && edibleByCarnivores();
+    }
 }
