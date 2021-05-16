39e02e41ba48190a4c8c0c558dd28f92630bcc75
jang0018 <jang0018@student.monash.edu>
2021-05-16 19:28:08 +0800

    changes to demo due to using ground interface

--- src/edu/monash/fit2099/demo/mars/Floor.java
+++ src/edu/monash/fit2099/demo/mars/Floor.java
@@ -1,11 +1,22 @@
 package edu.monash.fit2099.demo.mars;
 
 import edu.monash.fit2099.engine.Ground;
+import game.dinosaurs.CapableActor;
 
 
 public class Floor extends Ground {
 
 	public Floor() {
 		super('.');
+	}
+
+	@Override
+	public boolean canLayEggHere(CapableActor capableActor) {
+		return false;
+	}
+
+	@Override
+	public boolean canBreedHere(CapableActor capableActor) {
+		return false;
 	}
 }
