f6b093465aa934f244188b3169003230bb784c58
jang0018 <jang0018@student.monash.edu>
2021-05-19 12:11:23 +0800

    add EvadeDinoBehaviour class

---
+++ src/game/movement/EvadeDinoBehaviour.java
@@ -0,0 +1,49 @@
+package game.movement;
+
+import edu.monash.fit2099.engine.*;
+import game.Behaviour;
+import game.attack.Corpse;
+import game.dinosaurs.Pterodactyl;
+
+public class EvadeDinoBehaviour implements Behaviour {
+
+    public EvadeDinoBehaviour() {
+    }
+
+    @Override
+    public Action getAction(Actor actor, GameMap map) {
+        if (actor instanceof Pterodactyl){
+            Location location = map.locationOf(actor);
+            boolean hasCorpse = false;
+            for (Item item : location.getItems()){
+                if(item instanceof Corpse){
+                    hasCorpse = true;
+                    break;
+                }
+            }
+
+            if (!hasCorpse){
+                return null;
+            }
+
+            boolean shouldMoveAway = false;
+            Location moveToLocation = null;
+            for (Exit exit: location.getExits()){
+                if (location.containsAnActor()){
+                    shouldMoveAway = true;
+                }
+                else {
+                    moveToLocation = exit.getDestination();
+                }
+
+                if (shouldMoveAway && moveToLocation != null){
+                    return new DynamicMoveAction(moveToLocation, exit.getName());
+                }
+            }
+
+
+        }
+
+        return null;
+    }
+}
