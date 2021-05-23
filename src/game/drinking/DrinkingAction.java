d75032d00ed80344f82010dc4dbc75c054ce6d48
JONATHAN_WONG_LEONG_SHAN <jwon0100@student.monash.edu>
2021-05-23 12:33:02 +0800

    Added Javadoc for Drinking package

--- src/game/drinking/DrinkingAction.java
+++ src/game/drinking/DrinkingAction.java
@@ -6,14 +6,31 @@ import edu.monash.fit2099.engine.GameMap;
 import game.dinosaurs.DinoActor;
 import game.environment.DrinkingGround;
 
+/**
+ * Special Action for drinking from lakes.
+ */
 public class DrinkingAction extends Action {
 
+    /**
+     * The lake that DinoActors can drink out of
+     */
     private final DrinkingGround drinkingGround;
 
+    /**
+     * Constructor.
+     *
+     * @param drinkingGround the lake to drink from
+     */
     public DrinkingAction(DrinkingGround drinkingGround){
         this.drinkingGround = drinkingGround;
     }
 
+    /**
+     * Performs the drinking action
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return Description of the drinking action.
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         if(actor instanceof DinoActor){
--- src/game/drinking/DrinkingBehaviour.java
+++ src/game/drinking/DrinkingBehaviour.java
@@ -5,10 +5,25 @@ import game.Behaviour;
 import game.dinosaurs.DinoActor;
 import game.environment.DrinkingGround;
 
+/**
+ * Simulates drinking behaviour of DinoActors from lakes
+ * depending on whether certain criteria is met.
+ */
 public class DrinkingBehaviour implements Behaviour {
 
+    /**
+     * Constructor.
+     *
+     */
     public DrinkingBehaviour(){}
 
+    /**
+     * Returns DrinkingAction if the actor is allowed to drink from a lake by checking
+     * necessary conditions, otherwise null is returned.
+     * @param actor the Actor acting
+     * @param map the GameMap containing the Actor
+     * @return DrinkingAction if actor can drink from lake, otherwise null is returned
+     */
     public Action getAction(Actor actor, GameMap map){
         if (actor instanceof DinoActor) {
             DinoActor dinoActor = (DinoActor) actor;
